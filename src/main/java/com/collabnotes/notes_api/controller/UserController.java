a0518a8788199da7e56b2e5b6ef37826ea526add
joaquinmezzano <mezzanojoaquin@gmail.com>
2025-09-24 19:11:16 -0300

    GET /users, GET /users/{id}

--- src/main/java/com/collabnotes/notes_api/controller/UserController.java
+++ src/main/java/com/collabnotes/notes_api/controller/UserController.java
@@ -3,8 +3,11 @@ package com.collabnotes.notes_api.controller;
 import com.collabnotes.notes_api.model.User;
 import com.collabnotes.notes_api.repository.UserRepository;
 import jakarta.validation.Valid;
+import org.springframework.expression.spel.ast.NullLiteral;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -24,5 +27,23 @@ public class UserController {
 
         User saved = userRepository.save(user);
         return ResponseEntity.ok(saved);
+    }
+
+    // GET /users -> Get all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    // GET /users/{id} -> Get the user from the id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
